e5111f876be114cb6e1130a3ba672653b5905818
AbdiHaryadi <abdiharyadi.ah@gmail.com>
2024-02-17 12:00:40 +0800

    feat: add "/invalid-endpoints" endpoint

--- endpoint/main.py
+++ endpoint/main.py
@@ -1,15 +1,30 @@
-from typing import Union
-
 from fastapi import FastAPI
 
 app = FastAPI()
-
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/invalid-endpoints")
+def read_invalid_endpoints():
+    return {
+        "timestamp": 213213,
+        "data": [
+            {
+                "endpoint_tps": "http://pemilu2024.kpu.go.id/pilpres/hitung-suara/34/3403/340309/3403092003/3403092003001",
+                "assertion_error": [
+                    "Assertion Error 1",
+                    "Assertion Error 2",
+                    "Assertion Error 3",
+                ]
+            },
+            {
+                "endpoint_tps": "https://pemilu2024.kpu.go.id/pilpres/hitung-suara/73/7371/737111/7371111005/7371111005001",
+                "assertion_error": [
+                    "Assertion Error 2",
+                    "Assertion Error 4"
+                ]
+            }
+        ]
+    }
